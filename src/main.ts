00c7627f9c4203befd2ea4e91ed56c00d20e7bf6
Cotton Hou <himcotton@gmail.com>
2024-03-12 06:48:21 +0800

    extract main out from mod

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import ssh from 'ed25519-keygen/ssh';
+
+import { PBKDF2 } from './pbkdf2.ts';
+
+import { encode } from './utils.ts';
+
+
+
+
+
+export type Keys = Readonly<{
+    publicKeyBytes: Uint8Array;
+    publicKey: string;
+    fingerprint: string;
+    privateKey: string;
+}>;
+
+
+
+
+
+export async function main ([
+
+        salt,
+        passphrase = 'the default passphrase',
+
+]: ReadonlyArray<string>): Promise<Keys> {
+
+    if (salt == null || salt.trim().length < 5) {
+        throw new Error('invalid salt, at least 5 characters long');
+    }
+
+    const entropy = await PBKDF2({
+        salt: encode(salt),
+        passphrase: encode(passphrase),
+    });
+
+    return ssh(new Uint8Array(entropy));
+
+}
+
